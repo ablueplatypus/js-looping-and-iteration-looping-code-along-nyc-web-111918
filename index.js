c0cb1a6154e217f942aafab5ce277d16b9785ab1
Matt Doering <ablueplatypus21@gmail.com>
2019-01-07 13:49:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+// function printBadges(arr){
+//
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(i);
+//     // let message = `Welcome ${arr[i]}! You are employee #${i + 1}.`
+//     // console.log(message);
+//   }
+// }
+function printBadges(arr){
+
+  for (let i = 0; i < arr.length; i++) {
+    let message = `Welcome ${arr[i]}! You are employee #${i + 1}.`
+    console.log(message);
+  }
+  return arr;
+}
+
+function tailsNeverFails() {
+  let count = 0;
+    while (Math.random() >= 0.5){
+      count++;
+    }
+    return `You got ${count} tails in a row!`;
+}
